501220101c3b6f87c6fec3bc25ebbf39c5a8f875
Lucas Pinheiro <lucaspinheiru@hotmail.com>
2021-03-01 19:44:43 +0000

    assertEqual initial file

---
+++ assertEqual.js
@@ -0,0 +1,12 @@
+const assertEqual = function(actual,expected) {
+  if (actual === expected) {
+    console.log("😀😀😀 Assertion Passed: " + actual + " === " + expected + ".");
+  }   else {
+    console.log("😩😩😩 Assertion Failed: " + actual + " !== " + expected + ".");
+  }
+};
+
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1,1);
+assertEqual(1, "1");
+assertEqual("Lucas", "Lucas");
