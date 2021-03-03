415cb3433395f59ca2a5b10596814066f0d52aad
Lucas Pinheiro <lucaspinheiru@hotmail.com>
2021-03-03 23:30:18 +0000

    findKeyByValeu created

--- letterPositions.js
+++ letterPositions.js
@@ -25,18 +25,19 @@ const eqArrays = function(firstArr, secondArr) {
   const letterPositions = function(sentence) {
     const results = {};
     
-    for (let i = 0; i < sentence.length; i++) {
-      if (sentence[i] !== ' ') {
-        if (! results[sentence[i]]) {
+    for(let i = 0; i < sentence.length; i++){
+      if(sentence[i] !== ' ') {
+        if(! results[sentence[i]]){
           results[sentence[i]] = [i];
         } else {
-          results[sentence[i]].push(i);     
-          
-          }       
+          results[sentence[i]].push(i);
+          }
       }
     }
     return results;
-  };
+  }
+  
+
 assertArraysEqual(letterPositions("hello").e, [1]);
 // assertArraysEqual(letterPositions("hello").o, [4]);
 // assertArraysEqual(letterPositions("hello").h, [1]);
