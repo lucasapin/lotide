36a13cfd684e8d99a225405f4577a44ab52d95fe
Lucas Pinheiro <lucaspinheiru@hotmail.com>
2021-03-04 20:40:19 +0000

    map.js implemented

---
+++ assertObjectsEqual.js
@@ -0,0 +1,48 @@
+const eqArrays = function(firstArr, secondArr) {
+  if(firstArr.length !== secondArr.length){
+    return false
+  } else {
+    for(var i = 0; i < firstArr.length; i++){
+      if(firstArr[i] !== secondArr[i]){
+    return false
+  } 
+}
+  return true;
+}
+}
+
+const eqObjects = function(object1, object2) {
+  let object1Keys = Object.keys(object1).sort();
+  let object2Keys = Object.keys(object2).sort();
+  let object1Values = Object.values(object1).sort();
+  let object2Values = Object.values(object2).sort();
+
+  if(object1Keys.length !== object2Keys.length) {
+    return false;
+    } else if(eqArrays(object1Keys, object2Keys) && eqArrays(object1Values, object2Values)) {
+      return true;
+    }
+};
+
+const assertObjectsEqual = function(actual, expected) {
+  const inspect = require('util').inspect; // <= add this line
+  // ...
+  // console.log(`Example label: ${inspect(expected)}`);
+
+    if (eqObjects(actual, expected)) {
+      console.log(`😀😀😀 Assertion Passed: ${actual} === ${expected}.`);
+    } else {
+      console.log(`😩😩😩 Assertion Failed: ${actual} !== ${expected}.`);
+      } 
+};
+
+const ab = { a: "3", b: "2", c: [1, 2, 3] };
+const ba = { a: "3", b: "2", c: [1, 2, 3] };
+console.log(assertObjectsEqual(ab,ba));
+
+  
+// assertEqual(eqObjects(ab, ba), true); // => true
+
+// const abc = { a: "1", b: "2", c: "3" };
+// assertEqual(eqObjects(ab, abc), false); // => false
+
---
+++ map.js
@@ -0,0 +1,34 @@
+const eqArrays = function(firstArr, secondArr) {
+  if(firstArr.length !== secondArr.length){
+    return false
+  } else {
+    for(var i = 0; i < firstArr.length; i++){
+      if(firstArr[i] !== secondArr[i]){
+    return false
+  } 
+}
+  return true;
+}
+};
+  
+  
+  const assertArraysEqual = function(arrOne, arrTwo) {
+    if (eqArrays(arrOne, arrTwo)){
+      console.log("😁 Assertion Passed:" , arrOne , "===" , arrTwo);
+  
+    } else {
+      console.log("😩 Assertion Failed:" , arrOne , "!==" , arrTwo);
+    }
+  
+  }; 
+
+const words = ["ground", "control", "to", "major", "tom"];
+const map = function(array, callback) {
+  const results = [];
+  for (let item of array) {
+    results.push(callback(item));
+    }
+  return results;
+}
+
+assertArraysEqual(map(words, word => word[1]), ['r', 'o', 'o', 'a', 'o']);
