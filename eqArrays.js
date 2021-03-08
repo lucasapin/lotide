fcddc6bf466abb9a6bc39fb3ef38f6c72eb0c4ed
Lucas Pinheiro <lucaspinheiru@hotmail.com>
2021-03-08 14:48:29 +0000

    eqArrays refactored

--- eqArrays.js
+++ eqArrays.js
@@ -1,3 +1,4 @@
+const assertEqual = require("./assertEqual")
 const eqArrays = function(firstArr, secondArr) {
   if(firstArr.length !== secondArr.length){
     return false
@@ -9,14 +10,7 @@ const eqArrays = function(firstArr, secondArr) {
 }
   return true;
 }
-}
-
-const assertEqual = function(actual,expected) {
-  if (actual === expected) {
-    console.log(`😀😀😀 Assertion Passed: ${actual} === ${expected}.`);
-  }   else {
-    console.log(`😩😩😩 Assertion Failed: ${actual} !== ${expected}.`);
-  }
 };
 
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+module.exports = eqArrays;
+
--- head.js
+++ head.js
@@ -1,8 +1,8 @@
-const assertEqual = require('./assertEqual');
+const assertEqual = require("./assertEqual");
 
 const head = function(array) {
   return array[0];
 }
 
-
+assertEqual(head([1,2,3].length),3);
 module.exports = head;
